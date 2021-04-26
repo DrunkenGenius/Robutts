430676c5a9d90e2577d174c034c49aabad5f4a3b
DrunkenGenius <troelsdeibjerg@gmail.com>
2021-04-26 16:33:10 +0200

    Revert "Update Robutts.cpp"
    This reverts commit 788bb737cff5c1c3021b60a031914c48a74969b9.

--- ev3dev-lang-cpp/demos/Robutts.cpp
+++ ev3dev-lang-cpp/demos/Robutts.cpp
@@ -264,7 +264,7 @@ void control::mainControl()
       std::cout << "BumperSensor triggered" << std::endl;
       driveBackwards(500, 1000);
       usleep(1 * microsecond);
-      turnLeft(500, 1000);
+      turnLeft(500, 400);
       usleep(1 * microsecond);;
     }
 
